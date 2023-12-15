6e87d8c8e85a20cbc313aedcb6c0cef4c07a719a
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-15 15:29:55 +0000

    [add] #2 Variable_Chapter_sample.js letによる変数の再代入

--- sample.js
+++ sample.js
@@ -1,5 +1,12 @@
 // varによる再代入
-var nickname = "taro";
+// var nickname = "taro";
+// console.log(nickname);
+
+// nickname = "jiro";
+// console.log(nickname);
+
+// letによる再代入
+let nickname = "taro";
 console.log(nickname);
 
 nickname = "jiro";
